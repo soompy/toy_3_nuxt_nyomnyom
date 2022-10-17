6866d4346294afe7c27e7b234cb148907dc1d168
KAKAOVX <>
2022-10-17 16:53:43 +0900

    메인 헤더내 아이콘 버튼 컴포넌트추가

--- components/Header/MainHeader.vue
+++ components/Header/MainHeader.vue
@@ -1,90 +1,52 @@
 <template>
     <header id="header">
         <div class="inner">
-            <h1 class="logo"><a href="">logo</a></h1>
-            <nav>
-                <div class="nav_bar">
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                </div>
-                <!-- nav_bar 클릭 시 gnb에 on 클래스 추가 -->
-                <ul class="gnb">
-                    <li><a href="">1</a></li>
-                    <li><a href="">2</a></li>
-                    <li><a href="">3</a></li>
-                </ul>
-            </nav>
+            <h1 class="logo"><a href="">logo</a></h1>            
+            <div class="button-container">
+                <ButtonIcon class="icon ic_search black"/>
+                <ButtonIcon class="icon ic_share grey"/>                
+            </div>
         </div>
     </header>
 </template>
 
 <script>
+import ButtonIcon from '../button/buttonIcon.vue';
+
 export default {
     name: "MainHeader",
-    data: {
-        
+    components: { 
+        ButtonIcon
     },
-    methods: {
+    // data: {
         
-    },
+    // },
+    // methods: {
+        
+    // },
 };
 </script>
 
 <style lang="scss" scoped>
-#header {
-    position: fixed;
-    left: 0;
-    top: 0;
-    width: 100%;
-    background: #ffffff;
-
-    .inner {
-        display: flex;
-        align-items: center;
-        position: relative;
+    #header {
+        position: fixed;
+        left: 0;
+        top: 0;
         width: 100%;
-        padding: 10px 16px;
-    }
+        background: #ffffff;
 
-    nav {
-        position: relative;
-        width: 22px;
-        height: 22px;
-        margin-left: auto;
-
-        .nav_bar {
+        .inner {
             display: flex;
-            flex-direction: column;
-            justify-content: space-around;
+            align-items: center;
             position: relative;
             width: 100%;
-            height: 100%;
-
-            .bar {
-                display: inline-block;
-                width: 100%;
-                height: 2px;
-                background: #333333;
-                border-radius: 16px;
-            }
+            padding: 10px 16px;
         }
-
-        .gnb {
-            display: none;
-            position: fixed;
-            left: 0;
-            top: 0;
-            width: 100%;
-            background: #ffffff;
-            li {
-                padding: 8px 16px;
-                border-bottom: 1px solid #cccccc;                
-            }
-            &.on {
-                display: block;
-            }
+        .button-container {
+            display: flex;
+            align-content: center;
+            justify-content: center;
+            margin-left: auto;
         }
     }
-}
 </style>
---
+++ components/button/buttonIcon.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="button_icon"></button>
+</template>
+
+<script>
+export default {
+    name: "ButtonIcon",
+    data: {
+
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .button_icon {
+        position: relative;
+        width: 24px;
+        height: 24px;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: 100%;         
+    }
+</style>
