193de6d90836654c19cb62ff4e6826c7e266b9b8
KAKAOVX <>
2023-08-16 08:25:50 +0900

    불필요 파일 정리

--- components/widgets/card/InterestsCard.vue
+++ components/widgets/card/InterestsCard.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="interests-content">
-    <!-- swiper 사용 -->
-    
+  <div>
+    슬라이드ui
   </div>
 </template>
 
 <script>
-
 export default {
-  name: "InterestsCard",
-  components: {
-    
-  },
+  components: {},
   data() {
     return {
-              
+      sliderItems: [
+        {
+          image: "/assets/images/interest_00.png",
+          title: "Slide 1",
+          description: "Description for Slide 1",
+        },
+        {
+          image: "/assets/images/interest_01.png",
+          title: "Slide 2",
+          description: "Description for Slide 2",
+        },
+        // 필요한 만큼 슬라이드 항목을 추가
+      ],
     };
   },
 };
 </script>
-
-<style lang="scss" scoped></style>
